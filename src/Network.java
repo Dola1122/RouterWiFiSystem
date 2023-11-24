b7b54b22f54d1cb6d14f39ed9932d864fe352618
Roaa Moustafa <75594455+roaamoustafaa@users.noreply.github.com>
2023-11-24 15:58:53 +0200

    Network.java

--- src/Network.java
+++ src/Network.java
@@ -1,7 +1,30 @@
+import java.util.ArrayList;
+import java.util.Scanner;
 
+import static java.lang.Thread.sleep;
 
 public class Network {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
+        int  numberOfConnections, numberOfDecives;
+        ArrayList<Device> devices = new ArrayList<>();
 
+        try (Scanner input = new Scanner(System.in)) {
+             System.out.println(" ");
+            System.out.print("Enter number of WI-FI Connections: ");
+            numberOfConnections = input.nextInt();
+            Router router = new Router(numberOfConnections);
+            numberOfDecives = input.nextInt();
+            System.out.println("Enter device name and type");
+            for (int i = 0; i < numberOfDecives; i++) {
+                System.out.print("Enter device("+(i+1)+") : ");
+                Device newDevice = new Device(input.next(),input.next(), router);
+                devices.add(newDevice);
+            }
+        }
+
+        for (int i = 0; i < numberOfDecives; i++) {
+            sleep(100);
+            devices.get(i).start();
+        }
     }
 }
