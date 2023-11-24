b17cd83e0a2d69627b86b3aeb1aea4ad0903c31c
Roaa Moustafa <75594455+roaamoustafaa@users.noreply.github.com>
2023-11-24 15:59:31 +0200

    Semaphore.java

--- src/Semaphore.java
+++ src/Semaphore.java
@@ -1,20 +1,53 @@
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.FileWriter;
 
 public class Semaphore {
-    private int permits;
+    private int permits = 0;
 
-    public Semaphore(int permits) {
-        this.permits = permits;
+    public Semaphore() {
+        permits = 0;
     }
 
-    public synchronized void acquire() throws InterruptedException {
-        while (permits == 0) {
-            wait();
-        }
+    public Semaphore(int initialPermits) {
+        permits = initialPermits;
+    }
+
+    public int getPermits() {
+        return permits;
+    }
+
+    public synchronized void acquire(Device device) throws IOException {
         permits--;
+        if (permits < 0) {
+            try {
+                FileWriter fileWriter = new FileWriter("output.txt", true);
+                PrintWriter printWriter = new PrintWriter(fileWriter);
+                
+                printWriter.println(device.getDeviceName() + " (" + device.getDeviceType() + ") arrived and waiting");
+                printWriter.close();
+                
+                wait();
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+        } else {
+            try {
+                FileWriter fileWriter = new FileWriter("output.txt", true);
+                PrintWriter printWriter = new PrintWriter(fileWriter);
+                
+                printWriter.println(device.getDeviceName() + " (" + device.getDeviceType() + ") arrived");
+                printWriter.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
     public synchronized void release() {
         permits++;
-        notify();
+        if (permits <= 0) {
+            notify();
+        }
     }
 }
