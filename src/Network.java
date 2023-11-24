83b7e8616c18d6dd680189834962a5875e94ace9
Dola1122 <dolagamer2002@gmail.com>
2023-11-24 16:45:49 +0200

    added comments to the network class

--- src/Network.java
+++ src/Network.java
@@ -5,14 +5,6 @@ import java.io.File;
 import static java.lang.Thread.sleep;
 
 public class Network {
-//    File file = new File("output.txt");
-//    public Network(){
-//        if (file.exists()){
-//            file.delete();
-//        } else {
-//            System.out.println("no file");
-//        }
-//    }
 
     public static void main(String[] args) throws InterruptedException {
         File file = new File("output.txt");
@@ -20,25 +12,33 @@ public class Network {
         if (file.exists()){
             file.delete();
         }
-        int  numberOfConnections, numberOfDecives;
+        int numberOfConnections, numberOfDevices;
+
         ArrayList<Device> devices = new ArrayList<>();
 
         try (Scanner input = new Scanner(System.in)) {
-             System.out.println(" ");
-            System.out.print("Enter number of WI-FI Connections: ");
+            System.out.println(" ");
+            // Get the number of Wi-Fi connections from the user
+            System.out.print("Enter number of Wi-Fi Connections: ");
             numberOfConnections = input.nextInt();
+
+            // Create a router with the specified number of connections
             Router router = new Router(numberOfConnections);
+
+            // Get the number of devices the user wants to connect
             System.out.print("Enter number of devices Clients want to connect: ");
-            numberOfDecives = input.nextInt();
+            numberOfDevices = input.nextInt();
+
+            // Get information about each device from the user and add it to the ArrayList
             System.out.println("Enter device name and type");
-            for (int i = 0; i < numberOfDecives; i++) {
-                System.out.print("Enter device("+(i+1)+") : ");
-                Device newDevice = new Device(input.next(),input.next(), router);
+            for (int i = 0; i < numberOfDevices; i++) {
+                System.out.print("Enter device(" + (i + 1) + ") : ");
+                Device newDevice = new Device(input.next(), input.next(), router);
                 devices.add(newDevice);
             }
         }
 
-        for (int i = 0; i < numberOfDecives; i++) {
+        for (int i = 0; i < numberOfDevices; i++) {
             sleep(100);
             devices.get(i).start();
         }
