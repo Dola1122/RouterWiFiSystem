0871c9e966423134d3bb03f5124fa632fec22922
Roaa Moustafa <75594455+roaamoustafaa@users.noreply.github.com>
2023-11-24 15:58:23 +0200

    Device.java

--- src/Device.java
+++ src/Device.java
@@ -1,28 +1,67 @@
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.FileWriter;
 
 public class Device extends Thread {
-    private String name;
-    private String type;
-    private Router router;
+    private String deviceName, deviceType;
+    private int connectionID;
+    private Router deviceRouter;
 
     public Device(String name, String type, Router router) {
-        this.name = name;
-        this.type = type;
-        this.router = router;
+        deviceName = name;
+        deviceType = type;
+        deviceRouter = router;
+        connectionID = -1;
     }
 
-    public String getType() {
-        return type;
+    @Override
+    public void run() {
+        try {
+            deviceRouter.semaphore.acquire(this);
+            connectionID = deviceRouter.connect(this);
+            logToFile("Connection " + connectionID + ": " + deviceName + " Occupied");
+            performOnlineActivity();
+            deviceRouter.disconnect(this);
+            deviceRouter.semaphore.release();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void performOnlineActivity() throws InterruptedException, IOException {
+        logToFile("Connection " + connectionID + ": " + deviceName + " Login");
+        logToFile("Connection " + connectionID + ": " + deviceName + " performs online activity");
+        sleep(2000);
+    }
+
+    private void logToFile(String message) throws IOException {
+        try {
+            FileWriter fileWriter = new FileWriter("output.txt", true);
+            PrintWriter printWriter = new PrintWriter(fileWriter);
+
+            printWriter.println(message);
+            printWriter.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public int getConnectionID() {
+        return connectionID;
+    }
+
+    public void setConnectionID(int connectionID) {
+        this.connectionID = connectionID;
     }
 
     public String getDeviceName() {
-        return name;
+        return deviceName;
     }
 
-    public void performActivity(){
-
-    }
-    @Override
-    public void run() {
-
+    public String getDeviceType() {
+        return deviceType;
     }
 }
+
