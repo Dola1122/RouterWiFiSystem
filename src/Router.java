53ac12542d5de3b0c2fc4fed11cb482872f18011
Roaa Moustafa <75594455+roaamoustafaa@users.noreply.github.com>
2023-11-24 15:59:13 +0200

    Router.java

--- src/Router.java
+++ src/Router.java
@@ -1,23 +1,47 @@
-import java.util.LinkedList;
-import java.util.Queue;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.FileWriter;
 
 public class Router {
-    private Semaphore semaphore;
-    private Queue<Device> connections;
-    private Queue<Device> waitingDevices;
+    private boolean[] connectedDevices;
+    private int maxDeviceCount, currentConnectedDevices;
+    public Semaphore semaphore;
 
-    public Router(int maxConnections) {
-        semaphore = new Semaphore(maxConnections);
-        connections = new LinkedList<>();
-        waitingDevices = new LinkedList<>();
+    public Router(int maxDeviceCount) {
+        this.maxDeviceCount = maxDeviceCount;
+        semaphore = new Semaphore(maxDeviceCount);
+        connectedDevices = new boolean[maxDeviceCount];
     }
 
-    public void connect(Device device) throws InterruptedException {
+    public synchronized int connect(Device device) throws InterruptedException {
+        for (int i = 0; i < maxDeviceCount; i++) {
+            if (!connectedDevices[i]) {
+                currentConnectedDevices++;
+                device.setConnectionID(i + 1);
+                connectedDevices[i] = true;
+                Thread.sleep(100);
+                break;
+            }
+        }
+        return device.getConnectionID();
     }
-    public void disconnect(Device device) {
 
+    public synchronized void disconnect(Device device) {
+        currentConnectedDevices--;
+        connectedDevices[device.getConnectionID() - 1] = false;
+        logToFile("Connection " + device.getConnectionID() + ": " + device.getDeviceName() + " Logged out");
+        notify();
     }
-    public Queue<Device> getConnections() {
-        return connections;
+
+    private void logToFile(String message) {
+        try {
+            FileWriter fileWriter = new FileWriter("output.txt", true);
+            PrintWriter printWriter = new PrintWriter(fileWriter);
+
+            printWriter.println(message);
+            printWriter.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
